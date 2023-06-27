7e7b9aa59fd585563e9456e7015de2fcc012ffe3
Ednilson Amaral <ednilsonamaral.ti@gmail.com>
2023-06-26 22:12:28 -0300

    feat: prettier, libs, controller, prefix, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+// import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
+import { HeroesModule } from './heroes/heroes.module';
 
 @Module({
-  imports: [],
+  // imports: [HttpModule, InMemoryDBModule.forRoot({}), HeroesModule],
+  imports: [HeroesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      statusCode: 200,
+      statusText: 'OK',
+      message: 'Hello!',
+    };
   }
 }
